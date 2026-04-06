453bb45a1ffd83960f2181f0741c20d5385c7baf
Izabel-dev <admizagou@hotmail.com>
2026-04-06 20:42:31 -0300

    Update admin.py

--- times/admin.py
+++ times/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Time 
+from .models import Time
 
-admin.site.register(Time)
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cidade', 'fundacao')
+    list_filter = ('cidade',)
+    search_fields = ('nome',)
+
+admin.site.register(Time, TimeAdmin)
