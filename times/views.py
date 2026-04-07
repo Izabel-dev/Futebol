5cc2bf39fd2bb9a69a04dde2e2bad5d5721f49d7
Izabel-dev <admizagou@hotmail.com>
2026-04-07 19:51:11 -0300

    Update views.py

--- times/views.py
+++ times/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Time
 
 def lista_times(request):
-    times = Time.objects.all() 
+    times = Time.objects.all() .order_by('fundacao')
     return render(request, 'lista_times.html', {'times': times})  
